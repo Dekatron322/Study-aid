3af8adcafa44c296a1776d3af68e09e75b4dab62
Dekatron322 <muritalaibrahim097@gmail.com>
2021-03-05 00:43:22 +0100

    third commit

--- study_resource/urls.py
+++ study_resource/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
 	
 	path("share-study-resource/", views.ShareResourceView, name="share_resource"),
 	path("study-resource-detail/<int:id>/", views.ResourceDetailView, name="resource_detail"),
+	path("contribute-to-ebook/<int:id>/", views.ContributeFileResourceView, name="contribute_ebook"),
 	path("contribute-to-resource/<int:id>/", views.ContributeResourceView, name="contribute_resource"),
+	path("contribute-to-link/<int:id>/", views.ContributeLinkResourceView, name="contribute_link"),
+	path("chat-form/", views.ChatForumView, name="chat_forum")
+
 
 ]
--- study_resource/views.py
+++ study_resource/views.py
@@ -76,12 +76,106 @@ def ResourceDetailView(request, id):
 
 
 
+def ContributeFileResourceView(request, id):
+	study_resource = StudyResource.objects.get(id=id)
+
+	if request.method == "POST":
+
+		content = request.POST.get("content")
+		link = request.POST.get("link")
+
+		try:
+			file = request.FILES["study_file"]
+			if type(file) == "image":
+				study_resource_image = StudyResourceImageConnector(study_resource=study_resource, image=file)
+				study_resource_image.save()
+
+			elif type(file) == "pdf":
+				study_resource_ebook = StudyResourceEbookConnector(study_resource=study_resource, ebook=file)
+				study_resource_ebook.save()
+
+			else:
+				pass
+
+
+
+		except:
+			pass
+
+		if link == "":
+			link = Link.objects.create(title=link, link=link)
+			link.save()
+			study_resource_link = StudyResourceLinkConnector(study_resource=study_resource, link=link)
+			study_resource_link.save()
+
+		if content == "":
+			content = Content.objects.create(content=content)
+			content.save()
+			study_resource_content = StudyResourceContentConnector(study_resource=study_resource, content=content)
+			study_resource_content.save()
+
+		study_resource.save()
+
+
+		return HttpResponseRedirect(reverse("main:index"))
+
+		
+
+	else:
+		context = {"study_resource": study_resource}
+		return render(request, "study_resource/contribute_to_files.html", context)
+
+
 def ContributeResourceView(request, id):
 	study_resource = StudyResource.objects.get(id=id)
 
 	if request.method == "POST":
 
 		content = request.POST.get("content")
+
+
+		try:
+			file = request.FILES["study_file"]
+			if type(file) == "image":
+				study_resource_image = StudyResourceImageConnector(study_resource=study_resource, image=file)
+				study_resource_image.save()
+
+			elif type(file) == "pdf":
+				study_resource_ebook = StudyResourceEbookConnector(study_resource=study_resource, ebook=file)
+				study_resource_ebook.save()
+
+			else:
+				pass
+
+
+
+		except:
+			pass
+
+		
+		if content != "":
+			content = Content.objects.create(content=content)
+			content.save()
+			study_resource_content = StudyResourceContentConnector(study_resource=study_resource, content=content)
+			study_resource_content.save()
+
+		study_resource.save()
+
+
+		return HttpResponseRedirect(reverse("main:index"))
+
+		
+
+	else:
+		context = {"study_resource": study_resource}
+		return render(request, "study_resource/contribute.html", context)
+
+def ContributeLinkResourceView(request, id):
+	study_resource = StudyResource.objects.get(id=id)
+
+	if request.method == "POST":
+
+
 		link = request.POST.get("link")
 
 		try:
@@ -108,11 +202,6 @@ def ContributeResourceView(request, id):
 			study_resource_link = StudyResourceLinkConnector(study_resource=study_resource, link=link)
 			study_resource_link.save()
 
-		if content != "":
-			content = Content.objects.create(content=content)
-			content.save()
-			study_resource_content = StudyResourceContentConnector(study_resource=study_resource, content=content)
-			study_resource_content.save()
 
 		study_resource.save()
 
@@ -123,4 +212,9 @@ def ContributeResourceView(request, id):
 
 	else:
 		context = {"study_resource": study_resource}
-		return render(request, "study_resource/contribute.html", context)
+		return render(request, "study_resource/contribute_to_links.html", context)
+
+def ChatForumView(request):
+	context = {}
+	return render(request, "study_resource/chat_forum.html", context)
+
